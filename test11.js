e9abc2c4f25fa0fe7f00f7de3e7be14920299287
niuge <694838286@qq.com>
2022-08-12 00:14:57 +0800

    update

--- test11.js
+++ test11.js
@@ -1,30 +1,43 @@
 //ES6 导入
-//分别导入
-// import { add, sub } from './test10.js';
-// //使用
-// console.log(add(1, 2));
-// // console.log(sub(1, 2));
-// console.log(add);
-//统一导入
+//命名导入
+//命名导出可以使用*批量获取并赋值给保存导出集合的别名，而无须列出每个标识符：
+import { add as add2, sub as sub2 } from './test10.js';
+// import { add , sub as sub2 } from './test10.js';
+console.log(add(1, 2));
+console.log(sub2(1, 2));
+
 import * as test10 from './test10.js';
-// console.log(test10.add(1, 2));
-// console.log(test10.sub(1, 2));
-console.log(test10.default.add(1, 2));
-console.log(test10.default.sub(1, 2));
-// //可同时用两种方式导入
-// import { add as add1, sub as sub1 ,a } from './test10.js';
-// console.log(add1(1, 2));
-// console.log(sub1(1, 2));
-// console.log(a);
+console.log(test10.add(1, 2));
+console.log(test10.sub(1, 2));
+console.log(test10.a);
 
-// //默认导出多个
-// import test10, { add as add2, sub as sub2 } from './test10.js';
-// console.log(add2(1, 2));
-// console.log(sub2(1, 2));
-// console.log(test10.a);
-// //默认导入
-// import test10 from './test10.js';
-// console.log(test10.add(1, 2));
 
-console.log(this); // undefined
+
+
+// 默认导入
+import test100 from './test10.js';
+console.log(test100.c);
+
+import {default as test101} from './test10.js';//与上面的区别是，这里使用了别名 等价
+console.log(test101.c);
+
+//同时使用默认导入和命名导入
+import test102, { add as add3, sub as sub3 } from './test10.js';
+console.log(add3(1, 2));
+console.log(sub3(1, 2));
+console.log(test102.a); //不存在返回undefined
+console.log(test102.c);
+
+import{default as test103, add , sub as sub4} from './test10.js';
+console.log(add(1, 2));
+console.log(sub4(1, 2));
+console.log(test103.c);
+import test104, * as test105 from './test10.js';
+console.log(test104.c);
+console.log(test105.a);
+console.log(test105.a);
+
+
+
+
 
--- test9.js
+++ test9.js
@@ -1,6 +1,8 @@
 //test9.js 导入test8.js
 //分别导入 add 和 sub
 var test8 = require('./test8');
+console.log(test8.add(1, 7));
+console.log(test8.sub(1, 7));
 //统一导入
 var {add , sub } = require('./test8');
 //可同时用两种方式导入
@@ -9,12 +11,12 @@ var {add:add1, sub:sub1 ,a } = require('./test8');
 console.log(add(1, 2));
 console.log(sub(1, 2));
 
-console.log(test8.add(1, 2));
-console.log(test8.sub(1, 2));
+// console.log(test8.add(1, 2));
+// console.log(test8.sub(1, 2));
 
 console.log(add1(1, 2));
 console.log(sub1(1, 2));
-console.log(a);
+// console.log(a);
 
 // 无论一个模块在 require()中被引用多少次，模块永远是单例。在下面的例子中，moduleA 只会
 // 被打印一次。这是因为无论请求多少次，moduleA 只会被加载一次。
